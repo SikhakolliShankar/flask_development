def1a48f90ea42950b2315b9f595f0fb77acaa34
Shankar <sikhakollishankar16@gmail.com>
2023-10-18 17:45:34 +0530

    configured SQLALCHEMY database (todo database)

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 from flask import Flask, render_template
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
-@app.route('/hello')
-def hello_world():
-    return "hello"
+app = Flask(__name__) 
 
+# sqlalchemy configuration 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def  __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+    
 @app.route('/')
 def htmlfile():
+    todo = Todo(title="First Todo", desc="Invest on time")
+    db.session.add(todo)
+    db.session.commit()
     return render_template("index.html")
 
 if __name__ == '__main__':
